210e32b6e31c7346e03dd72eec0caf37d3bd1673
Alex T Leach <alextleach@gmail.com>
2016-08-11 14:18:58 -0500

    it broke

--- app/game-store/x/controller.js
+++ app/game-store/x/controller.js
@@ -2,18 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   dialog: Ember.inject.service(),
-  flashMessages: Ember.inject.service(),
   cart: Ember.inject.service('cart'),
 
   actions: {
     addToCart(game) {
       this.get('dialog').createDialog({
         message: 'Are you sure?',
-      }).then(() => {
-        this.get('cart').addToCart(game);
-      }).catch(() => {
-        this.get('flashMessages').warning('We\'re so sorry to hear that...');
       });
+      // if (confirm('are you sure?')){
+      // this.get('cart').addToCart(game);
+    // }
     },
   },
 });
--- app/router.js
+++ app/router.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
-Router.map(function() {
-  this.route('game-store', function() {
+Router.map(function () {
+  this.route('game-store', {
+    path: '/',
+  }, function () {
     this.route('register');
     this.route('login');
   });
